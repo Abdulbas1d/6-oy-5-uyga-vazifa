4a1cb9917840bee579241c2fb66cd7008beb9b05
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-11-30 23:53:12 +0500

    Open files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import calculatorIcon from './assets/images/calculator-icon.svg'
+
+function App() {
+  return (
+    <div className='container'>
+      
+    </div>
+  )
+}
+
+export default App
