a35f9037a5a43b9c71669911f51b121ee6fdb171
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-12-02 16:18:54 +0500

    All codes write

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,77 @@ import './App.css'
 import calculatorIcon from './assets/images/calculator-icon.svg'
 
 function App() {
-  const [mortage, setMortage] = useState(null)
-  // const []
+  const [mortageInput, setMortageInput] = useState("")
+  const [mortageTerm, setMortageTerm] = useState("")
+  const [interestRate, setInterestRate] = useState("")
+  const [outputOne, setOutputOne] = useState('0')
+  const [outputTwo, setOutputTwo] = useState('0')
+
+  function validate() {
+    if (!mortageInput) {
+      alert("Biror bir miqdor kiritishingiz kerak!")
+      return false
+    }
+
+    if (!mortageTerm) {
+      alert("Necha yilga olishingizni ham kiritishingiz kerak!")
+      return false
+    }
+
+    if (!interestRate) {
+      alert("Necha foizga olishingizni ham kiritishingiz kerak!")
+      return false
+    }
+
+    return true
+  }
+
+  function outputWindow(event) {
+    event.preventDefault()
+
+    let isValid = validate()
+    if (!isValid) {
+      return
+    }
+
+    const monthlyInterestRate = interestRate / (100 * 12);
+    const totalMonths = mortageTerm * 12;
+    const resultOne = (mortageInput * monthlyInterestRate * (1 + monthlyInterestRate) ** totalMonths) / ((1 + monthlyInterestRate) ** totalMonths - 1);
+    const resultTwo = resultOne * totalMonths
+    setOutputOne(resultOne.toFixed(2))
+    setOutputTwo(resultTwo.toFixed(2))
+  }
+
+  function clearAll(event) {
+
+    event.preventDefault();
+    setMortageInput('');
+    setMortageTerm('');
+    setInterestRate('');
+    setOutputOne('0')
+    setOutputTwo('0')
+  }
+
   return (
     <div className='container'>
       <div className="container-left">
         <form className="form">
           <div className="title">
             <h2>Mortgage Calculator</h2>
-            <button className="btnn">Clear All</button>
+            <button className="btnn" type='button' onClick={clearAll}>Clear All</button>
           </div>
 
           <label className='amountLabel' htmlFor="amount">Mortgage Amount</label>
           <div className="amount">
             <h3>£</h3>
-            <input id='amount' type="number" />
+            <input id='amount' type="number" value={mortageInput} onChange={(e) => setMortageInput(e.target.value)} />
           </div>
 
           <div className="inputs">
             <div className="inputTerm">
               <label htmlFor='term'>Mortgage Term</label>
               <div className="inInput">
-                <input id='term' type="number" />
+                <input id='term' type="number" value={mortageTerm} onChange={(e) => setMortageTerm(e.target.value)} />
                 <h3 className='year'>years</h3>
               </div>
             </div>
@@ -32,7 +81,7 @@ function App() {
             <div className="inputRate">
               <label htmlFor="rate">Interest Rate</label>
               <div className="inInput-two">
-                <input id='rate' type="number" />
+                <input id='rate' type="number" value={interestRate} onChange={(e) => setInterestRate(e.target.value)} />
                 <h3>%</h3>
               </div>
             </div>
@@ -53,7 +102,7 @@ function App() {
               <span>Interest Only</span>
             </label>
           </div>
-          <button className="btn">
+          <button className="btn" onClick={(e) => outputWindow(e)}>
             <img src={calculatorIcon} alt="" />
             Calculate Repayments
           </button>
@@ -66,12 +115,12 @@ function App() {
 
         <div className="answersheet">
           <h4>Your monthly repayments</h4>
-          <h1>£1,797.74</h1>
+          <h1>£ {outputOne}</h1>
 
           <hr />
 
           <h4 className='over'>Total you'll repay over the term</h4>
-          <h2>£539,322.94</h2>
+          <h2>£ {outputTwo}</h2>
         </div>
       </div>
     </div>
